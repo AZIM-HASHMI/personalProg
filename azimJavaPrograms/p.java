245863aae1d46b1fd1801f74a1af30215df84a28
AZIM-HASHMI <AZIMTYMAIL@gmail.com>
2023-10-17 17:54:01 +0530

    new programs

---
+++ azimJavaPrograms/p.java
@@ -0,0 +1,9 @@
+package Swing;
+import javax.swing.*;
+public class p{
+    public static void main(String[] args) {
+        JFrame f = new JFrame("User form");
+        f.setVisible(true);
+        
+    }
+}
