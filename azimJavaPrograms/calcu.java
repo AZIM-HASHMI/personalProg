ca6a0651bfc8f58748ebe7348d2b3c4fe2000273
AZIM-HASHMI <AZIMTYMAIL@gmail.com>
2023-10-22 07:05:17 +0530

    SOLVING VS CODE PROBLEMS

--- azimJavaPrograms/calcu.java
+++ azimJavaPrograms/calcu.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class calc{  //USING SWITCH STATEMENT FOR DOING VARIOUS OPERATION / CALCULATOR
+public class calcu{  //USING SWITCH STATEMENT FOR DOING VARIOUS OPERATION / CALCULATOR
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Numbers");
